02d733cdbaa9dc3cc3d62c55c8bda945ced385df
Virag Patel <19it114@charusat.edu.in>
2021-02-12 10:12:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+while True:
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame,0)
+    detections = cascade_classifier.detectMultiScale(frame, 1.3, 5)
+
+    if(len(detections) > 0):
+        (x,y,w,h) = detections[0]
+        frame = cv2.rectangle(frame, (x,y),(x+w,y+h),(255,255,255),2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.release()
+cv2.destroyAllWindows()
